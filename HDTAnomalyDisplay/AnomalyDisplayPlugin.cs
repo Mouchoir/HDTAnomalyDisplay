abd61f7858d8117be3d3dd81da150b046cd12747
Mouchoir <4174327+Mouchoir@users.noreply.github.com>
2023-10-29 02:17:00 +0100

    feat: display basic anomaly card (#1)
    * feat: plugin initialization with basic anomaly card display
    
    * build: change post-build path from absolute to relative

---
+++ HDTAnomalyDisplay/AnomalyDisplayPlugin.cs
@@ -0,0 +1,52 @@
+﻿using Hearthstone_Deck_Tracker.API;
+using ControlCard = Hearthstone_Deck_Tracker.Controls.Card;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.Plugins;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+using System;
+using System.Windows.Controls;
+using static Hearthstone_Deck_Tracker.Windows.MessageDialogs;
+
+namespace HDTAnomalyDisplay
+{
+    public class AnomalyDisplayPlugin : IPlugin
+    {
+        public string Name => "HDTAnomalyDisplay";
+
+        public string Description => "Displays the current anomaly on your overlay";
+
+        public string ButtonText => "NO SETTINGS";
+        // public string ButtonText => Strings.GetLocalized("");
+
+        public string Author => "Mouchoir";
+
+        public Version Version => new Version(0, 1);
+
+        public MenuItem MenuItem => null;
+
+        private AnomalyDisplay anomalyDisplay;
+
+        public void OnButtonPress()
+        {
+        }
+
+        public void OnLoad()
+        {
+            anomalyDisplay = new AnomalyDisplay();
+            GameEvents.OnGameStart.Add(anomalyDisplay.HandleGameStart);
+            /*GameEvents.OnTurnStart.Add())*/
+
+            GameEvents.OnGameEnd.Add(anomalyDisplay.UpdateViewWithDefaultCard);
+
+        }
+
+        public void OnUnload()
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+    }
+}
---
+++ HDTAnomalyDisplay/AnomalyTooltipPanel.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Hearthstone_Deck_Tracker.Enums.Hearthstone;
+using Hearthstone_Deck_Tracker;
+using System;
+using static Hearthstone_Deck_Tracker.Windows.MessageDialogs;
+using System.Windows.Controls;
+
+namespace HDTAnomalyDisplay
+{
+    public partial class AnomalyTooltipPanel : UserControl, IDisposable
+    {
+
+        public AnomalyTooltipPanel()
+        {
+/*            InitializeComponent();
+            Settings.Load();
+
+            resize = new Resize(this, ReconnectButton, ReconnectText, Settings.Instance.reconnect);
+
+            Visibility = Visibility.Collapsed;
+            oriBrush = ReconnectButton.Background;
+
+            resize.AddToOverlayWindowPrivate();
+            resize.UpdatePosition();
+
+            updatePositionHandler = new RoutedEventHandler((sender, e) =>
+            {
+                resize.UpdatePosition();
+            });
+            Core.OverlayCanvas.AddHandler(SizeChangedEvent, updatePositionHandler);*/
+        }
+
+        public void Dispose()
+        {
+/*            timer.Dispose();
+            Settings.Instance.reconnect = resize.settings;
+            Settings.Save();
+            resize.RemoveFromOverlayWindowPrivate();
+            Core.OverlayCanvas.RemoveHandler(SizeChangedEvent, updatePositionHandler);
+            connectionLogWatcher?.Stop();*/
+        }
+
+        public void OnUpdate()
+        {
+            // Show reconnect button:
+            // 1. When we want to resize to move it in menu
+            // 2. When we're in the match
+/*            Visibility = Core.Game.IsInMenu ? resize.resizeGrip.Visibility : Visibility.Visible;*/
+
+        }
+
+        private bool IsInMainOrBgMenu()
+        {
+            return Core.Game.CurrentMode == Mode.HUB || Core.Game.CurrentMode == Mode.BACON;
+        }
+    }
+}
